f40cdf233c4ba6313a842cc648f79ef8dc75b50d
geoff.hodgson <geoffroh@gmail.com>
2013-06-04 23:56:21 +1000

    Revert of controller change

--- app/controllers/caches_controller.rb
+++ app/controllers/caches_controller.rb
@@ -2,7 +2,7 @@ class CachesController < ApplicationController
 
   def create
     @caches = Cache.instances_for(CacheConfigMatcher.sort_urls_into_hashes(params))
-    render json: @caches.map{|c| CacheClearer.push_to_queue(c.basename, [c.as_hash]) }
+    render json: @caches.map{|cache| CacheClearer.push_to_queue(cache.basename, [cache]) }
   end
 
 end
