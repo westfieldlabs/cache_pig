2ce5fbc6377fa3972afce03958c0bcb9cae53d0e
geoff.hodgson <geoffroh@gmail.com>
2013-06-05 03:31:18 +1000

    CachesController#create responding with basic json info. +specs

--- app/controllers/caches_controller.rb
+++ app/controllers/caches_controller.rb
@@ -1,8 +1,27 @@
 class CachesController < ApplicationController
 
   def create
+    # for each url in the params, find which cache strategies it is applicable to
     @caches = Cache.instances_for(CacheConfigMatcher.sort_urls_into_hashes(params))
-    render json: @caches.map{|cache| CacheClearer.push_to_queue(cache.basename, [cache]) }
+
+    # Constructing response
+    resp = {}
+    CacheConfigMatcher.urls_as_array(params).map{|u| resp[u] = nil}
+
+    # Add cache objects to queues
+    @caches.each do |cache| 
+      queued = CacheClearer.push_to_queue(cache.basename, [cache])
+      cache.urls.each do |c|
+        resp[c]||=[]
+        resp[c] << (queued ? "Added to #{cache.strategy} queue" : "#{cache.basename} queue did not respond while adding #{c}")
+      end
+    end
+
+    # Respond with the outcome of each purge url in the params: queued/error/missing config
+    resp.each do |k,v|
+      resp[k] = v ? v.uniq : "Could not find cache config for '#{k}'"
+    end
+    render json: resp
   end
 
 end
---
+++ spec/api/caches_api_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+
+describe CachesController, :type => :api do
+
+  context "purging a single valid url" do
+
+    let(:url) { "/caches?url='http://www.acme.com/au/images/'" }
+
+    it "responds with the Added to X Queue message" do
+      post url
+      response.body.should eql("{\"http://www.acme.com/au/images/\":[\"Added to Cache::CloudFront queue\"]}")
+    end
+
+  end
+
+  context "purging multiple valid urls using multiple strategies" do
+
+    let(:url) { "/caches?url='http://www.acme.com/au/images/,http://cdnsa1.atwestfield.com/au/images/clusters/2013/banner/lbobo-may-m-z-strip.jpg'" }
+
+    it "responds with a Added to X Queue message for each" do
+      post url
+      response.body.should eql(
+        "{\"http://www.acme.com/au/images/\":[\"Added to Cache::CloudFront queue\"],\"http://cdnsa1.atwestfield.com/au/images/clusters/2013/banner/lbobo-may-m-z-strip.jpg\":[\"Added to Cache::CloudFront queue\",\"Added to Cache::Akamai queue\"]}"
+        )
+    end
+
+  end
+
+  context "purging a single valid url that uses multiple strategies" do
+
+    let(:url) { "/caches?url='http://www.westfield.com.au/au/images/clusters/2013/banner/lbobo-may-2914.jpg'" }
+
+    it "responds with the Added to X Queue message and lists both strategies" do
+      post url
+      response.body.should eql("{\"http://www.westfield.com.au/au/images/clusters/2013/banner/lbobo-may-2914.jpg\":[\"Added to Cache::Varnish queue\",\"Added to Cache::CloudFront queue\"]}")
+    end
+
+  end
+
+    
+  context "unsuccessful requests" do
+
+    let(:url) { "/caches?url='http://www.acme.com/au/images/'" }
+    let(:missing_url) { "caches?url='this-url-isnt-in-the-config-file'" }
+
+    it "provides a config missing message if the url isn't applicable to any strategies" do
+      post missing_url
+      response.body.should eql("{\"this-url-isnt-in-the-config-file\":\"Could not find cache config for 'this-url-isnt-in-the-config-file'\"}")
+    end
+
+    let(:bad_urls) { 
+      ["caches?urls='''''''''''''''''''''''''''",
+       "caches?urls=eval(@current_user)"
+      ]
+    }
+
+    it "doesnt die a horrible death when given bad urls" do
+      bad_urls.each do |bad_url|
+        post bad_url
+        response.body.should match("Could not find cache config")
+      end
+    end
+
+    context "an unresponsive queue" do # Not sure if this can happen or not?
+
+      before do
+        CacheClearer.stub(:push_to_queue).and_return(nil)
+      end
+
+      it "provides an informative message saying there was a problem" do
+        post url
+        response.body.should eql("{\"http://www.acme.com/au/images/\":[\"CloudFront queue did not respond while adding http://www.acme.com/au/images/\"]}")
+      end
+
+    end
+
+
+  end
+end
